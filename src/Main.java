b6e07de5b8052d6abf1c35e684800512d3aff083
Marta <85373964+Hinata1411@users.noreply.github.com>
2024-03-15 23:27:00 -0600

    Agregamos el main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Prueba Unitaria");
+
+        for (int i = 1; i <= 20; i++) {
+
+        }
     }
 }
