47dbda52fb29736a53b0c379dd18d0e5ac80f7d1
Marta <85373964+Hinata1411@users.noreply.github.com>
2024-03-15 23:04:42 -0600

    Creación de operaciones en clase Calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Calculadora {
+public class Calculadora {
+
+    public int sumar(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public int restar(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public int multiplicar(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    public double dividir(int num1, int num2) {
+        if (num2 == 0) {
+            throw new ArithmeticException("ERROR. No se puede dividir el número por cero");
+        }
+        return (double) num1 / num2;
+    }
+
+
 }
--- src/TestPruebaUnitaria.java
+++ src/TestPruebaUnitaria.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class TestPruebaUnitaria {
+public class TestPruebaUnitaria {
 }
